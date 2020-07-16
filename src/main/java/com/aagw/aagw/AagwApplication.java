128c5fa06ed552efa042caf176607cddb653b614
zhangpenghai <zhangpenghai@sczjxdqh.com>
2020-07-16 12:06:58 +0800

    工程架构

---
+++ src/main/java/com/aagw/aagw/AagwApplication.java
@@ -0,0 +1,13 @@
+package com.aagw.aagw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AagwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AagwApplication.class, args);
+    }
+
+}
